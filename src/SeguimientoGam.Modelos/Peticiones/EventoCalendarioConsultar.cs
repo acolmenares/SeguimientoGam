09755a1c2b7a599ae5cce84a2e4fa228137c6ae4
acolmenares <acolmenares@irdglobal.org>
2017-02-20 11:05:50 -0500

    diferente service worker para gams

--- src/SeguimientoGam.Modelos/Peticiones/EventoCalendarioConsultar.cs
+++ src/SeguimientoGam.Modelos/Peticiones/EventoCalendarioConsultar.cs
@@ -26,7 +26,13 @@ namespace SeguimientoGam.Modelos.Peticiones
         public DateTime? Fecha { get; set; }
 
     }
-
-
+    /*
+    SELECT e.* 
+     FROM `events` e 
+        join evento_macroactividad em on em.idevento= e.id 
+        join macroactividad ma on ma.idmacroactividad= em.idmacroactividad 
+        join lineaaccion la on la.idlineaaccion= ma.idlineaaccion 
+     WHERE la.idlineaaccion= 15 ORDER BY `e`.`id` DESC
+     */
 }
 
